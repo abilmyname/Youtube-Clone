a729702b66dc0d7b02ad9b9912896ca0335200a6
abilmyname <67348100+abilmyname@users.noreply.github.com>
2021-03-15 11:29:36 +0700

    Update Constants.swift

--- Youtube Clone/Constants.swift
+++ Youtube Clone/Constants.swift
@@ -4,11 +4,3 @@
 //
 //  Created by SLYTH on 15/03/21.
 //
-
-import Foundation
-
-struct Constants {
-  static let  API_KEY = "AIzaSyAVpl2CugmYOTgybzFFeh2zIPnJZbZJ6fo"
-  static let PLAYLIST_ID = "PLL32ppwoZBYLxol9-8cLD2sFMdRLsHCGb"
-  static let API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
-}
